2a5368f9bf584b66a992a77e8c75aabacdaea9b2
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2023-10-13 12:34:04 +0300

    task 3

---
+++ products-service-ts/serverless.ts
@@ -0,0 +1,41 @@
+import type { AWS } from '@serverless/typescript';
+
+import getProductsList from '@functions/getProductsList';
+import getProductById from '@functions/getProductById';
+
+const serverlessConfiguration: AWS = {
+  service: 'products-service-ts',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-auto-swagger'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs16.x',
+    stage: 'dev',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { getProductsList, getProductById },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node16',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
